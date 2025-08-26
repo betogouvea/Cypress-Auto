1d31d0bca0ebda71017a5859937cd72d9fa30563
betogouvea <beetoogouvea@gmail.com>
2025-08-26 09:50:54 -0300

    Create checkout-overview.ts

---
+++ cypress/e2e/checkout-overview.ts
@@ -0,0 +1,25 @@
+import { contains } from 'jquery';
+
+export class CheckoutOverview{
+  private readonly PAY_INFO: string = '[data-test="payment-info-value"]';
+  private readonly TOTAL_PRICE: string = '[data-test="total-label"]';
+  private readonly DESCRIPTION: string = '[data-test="title"]';
+  private readonly FINISH: string = '[data-test="finish"]';
+
+  description(value: string): void {
+    cy.get(this.DESCRIPTION)
+    .contains(value);
+  }
+  information(value: string): void {
+    cy.get(this.PAY_INFO)
+    .contains(value);
+  }
+  totalPrice(value: string): void {
+    cy.get(this.TOTAL_PRICE)
+    .contains(value);
+  }
+  finishBuy(): void {
+    cy.get(this.FINISH)
+    .first()
+    .click();
+  }
