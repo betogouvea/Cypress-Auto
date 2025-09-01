0beacac98d274215bd6ebc9342e46fc818978bb7
betogouvea <beetoogouvea@gmail.com>
2025-09-01 14:41:14 -0300

    update-mochawesome

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports/mochawesome",
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
-   // baseUrl: "", 
     setupNodeEvents(on, config) {
-      
+      return config;
     },
   },
 });
